a408a07f424e7fe78969fb06cc0287b36e257541
Matt Whalley <mattwhalley@Mac.attlocal.net>
2025-04-05 22:34:15 -0400

    add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://mttwhlly.github.io',
+  site: 'https://mattwhalley.com',
 });
