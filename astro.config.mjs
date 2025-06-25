c7f84e790ceb233d7ac2b6292d7b126ae80935bd
Matt Whalley <matt@mattwhalley.com>
2025-06-25 15:46:00 -0400

    Update astro config for static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static', // Changed from 'server' to 'static' to deploy as a static site
   server: {
     host: '127.0.0.1',
     port: 4321,
